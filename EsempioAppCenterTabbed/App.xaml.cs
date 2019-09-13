e329e223d580ff00a0e74d3873fa37ff583949db
Leo Armando - MAR.A <T952167@gdfnet.gdf.it>
2019-09-13 12:04:59 +0200

    Aggiungere i file di progetto.

---
+++ EsempioAppCenterTabbed/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using EsempioAppCenterTabbed.Services;
+using EsempioAppCenterTabbed.Views;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace EsempioAppCenterTabbed
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+            AppCenter.Start("android=95811093-a178-49bd-bded-8e690d24c311;" +
+                  "ios=56689543-a6e2-4fc0-b1c9-ec83ba385686;",
+                  typeof(Analytics), typeof(Crashes));
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ EsempioAppCenterTabbed/ViewModels/AboutViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AppCenter.Crashes;
+using System;
+using System.Collections.Generic;
+using System.Windows.Input;
+
+using Xamarin.Forms;
+
+namespace EsempioAppCenterTabbed.ViewModels
+{
+    public class AboutViewModel : BaseViewModel
+    {
+        public AboutViewModel()
+        {
+
+            try
+            {
+                Title = "About";
+                //Crashes.GenerateTestCrash();
+                OpenWebCommand = new Command(() => Device.OpenUri(new Uri("https://xamarin.com/platform")));
+            }
+            catch (Exception ex)
+            {
+                var dati = new Dictionary<string, string> { { "Nome","Armando"} };
+                Crashes.TrackError(ex);
+            }
+            
+        }
+
+        public ICommand OpenWebCommand { get; }
+    }
+}
